6cdf4198620526cefa94c2ec4bfb8dd93963a702
soldeace <barine@runbox.com>
2021-02-20 11:38:51 -0300

    map out problem

---
+++ src/main/kotlin/day12-inheritance.kt
@@ -0,0 +1,50 @@
+import java.util.*
+
+// Day 12: Inheritance
+//
+// Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video.
+// You are given two classes, Person and Student, where Person is the base class and Student is the derived class.
+// Completed code for Person and a declaration for Student are provided for you in the editor.
+// Observe that Student inherits all the properties of Person.
+// Complete the Student class by writing the following:
+//  - A Student class constructor which has 4 parameters: firstName (String), lastName(String), idNumber(Int), scores(Array<Int>)
+//  - A char calculate() method that calculates a Student object's average and returns the grade character representative
+//    of their calculated age (see table on Hackerrank's challenge page)
+
+open class Person (var firstName: String, var lastName: String, var idNumber: Int) {
+    // Print person data
+    fun printPerson() {
+        println(
+            """
+                Name: $lastName, $firstName
+                ID: $idNumber
+                """.trimIndent()
+        )
+    }
+}
+
+class Student(firstName: String,
+                       lastName: String,
+                       idNumber: Int,
+                       testScores: IntArray): Person(firstName, lastName, idNumber) {
+
+}
+
+internal object Solution {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val scan = Scanner(System.`in`)
+        val firstName = scan.next()
+        val lastName = scan.next()
+        val id = scan.nextInt()
+        val numScores = scan.nextInt()
+        val testScores = IntArray(numScores)
+        for (i in 0 until numScores) {
+            testScores[i] = scan.nextInt()
+        }
+        scan.close()
+        val s = Student(firstName, lastName, id, testScores)
+        s.printPerson()
+        System.out.println("Grade: " + s.calculate())
+    }
+}
